847397b6b91c79b091f112661dec9af07ec23f79
St4rCr0w <40279982+St4rCr0w@users.noreply.github.com>
2018-06-14 21:53:51 -0300

    Update ProjetoFinalAED.cpp

--- ProjetoFinalAED.cpp
+++ ProjetoFinalAED.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct eProduct{
+struct eProduct{ //struct para um produto
     string nomeP;
     int IDproduto;
     float preco;
@@ -10,16 +10,16 @@ struct eProduct{
     struct eProduct *anterior;
 };
 
-struct LISTAP{
+struct LISTAP{ //struct para uma lista com produtos
     eProduct *inicio,*fim;
 };
 
-struct CARGO{
+struct CARGO{ //struct para o cargo do funcionario
   string nameC;
   int chave;
 };
 
-struct CONTA{
+struct CONTA{ //struct para a conta do funcionario e cliente
 	string user,password;
 };
 
@@ -29,21 +29,21 @@ protected:
 	int totalClientes;
 	int totalJogos;
 public:
-	empresa(){
+	empresa(){ //construtor da empresa
 		totalFuncionarios = 0;
 		totalClientes = 0;
 		totalJogos = 0;
 	}
-	int getFunc(){
+	int getFunc(){ //retorna o total de funcionarios na empresa
 		return totalFuncionarios;
 	}
-	int getClientes(){
+	int getClientes(){//retorna o total de clientes da empresa
 		return totalClientes;
 	}	
-	int getTJogos(){
+	int getTJogos(){ //retorna o total de jogos na empresa
 		return totalJogos;
 	}
-	void changeFunc(bool opt){
+	void changeFunc(bool opt){ // adiciona ou subtrai do total de funcionarios
 		switch(opt){
 			case 0:
 				totalFuncionarios++;
@@ -54,7 +54,7 @@ public:
 		}
 
 	}
-	void changeClientes(bool opt){
+	void changeClientes(bool opt){ //adiciona ou subtrai do total de clientes
 		switch(opt){
 			case 0:
 				totalClientes++;
@@ -64,7 +64,7 @@ public:
 				break;
 		}
 	}
-	void changeJogos(bool opt){
+	void changeJogos(bool opt){ //adiciona ou subtrai do total de Jogos
 		switch(opt){
 			case 0:
 				totalJogos++;
@@ -74,21 +74,18 @@ public:
 				break;
 		}
 	}
-	void catalogo(){
-	    cout << "" << endl;
-	}
 };
 class produto: public empresa{
 protected:
-    LISTAP tlista;      
+    LISTAP tlista; //lista de produtos da empresa
 public:
 	
-	produto(){
+	produto(){ //construtor da classe
 		tlista.inicio = NULL;
 		tlista.fim = NULL;
 	}
 	
-    void setProduct(){
+    void setProduct(){ //adiciona um produto na lista de produtos da empresa(feita por um funcionario)
     	eProduct *newProduct;
     	newProduct = new eProduct;
     	cout << "Informe o nome do produto:\n" << endl;
@@ -104,32 +101,32 @@ public:
 		}
 		newProduct->proximo = NULL;
 		newProduct->anterior = NULL;
-		if(tlista.inicio = NULL){
-			tlista.inicio = newProduct;
-			tlista.fim = newProduct;
-		}else{
+		if(tlista.inicio = NULL){//se não tiver produto no catálogo
+			tlista.inicio = newProduct;//o produto entra como o primeiro
+			tlista.fim = newProduct;//o produto entra também como ultimo
+		}else{// se não, é adicionada no fim do catálogo
 			newProduct->anterior = tlista.fim;
 			tlista.fim->proximo = newProduct;
 			tlista.fim = newProduct;
 		}
     }
     
-	void catalogoProduto(){
+	void catalogoProduto(){ //apresenta o catálogo de produtos da empresa
 	    eProduct *aux;
 	    aux = new eProduct;
 	    aux = tlista.inicio;
 	    cout << "Catalogo de Produtos" << endl;
-	    cout << "Codigo" << "	|	" <<"Nome" << "	|	" << "Pre�o" << "	|	" << "Quantidade";
+	    cout << "Codigo" << "	|	" <<"Nome" << "	|	" << "Preço" << "	|	" << "Quantidade";
 	    while(aux != NULL){
 	        cout << aux -> IDproduto << "	|	" << aux -> nomeP << "	|	" << aux -> preco << "	|	" << aux -> QTD << endl;
 	        aux = aux -> proximo;
 	    }
 	}
-	bool existe(int IDproduto){
+	bool existe(int IDproduto){ // verifica se existe o produto no catálogo
 		eProduct  *auxiliar;
 		auxiliar = new eProduct;
 		auxiliar = tlista.inicio;
-		while(auxiliar != NULL){
+		while(auxiliar != NULL){//percorre o catálogo
 			if(auxiliar->IDproduto == IDproduto){
 				return true;
 			}
@@ -138,7 +135,7 @@ public:
 		return false;	
 	}
 	
-	string getNomeP(int IDproduto){
+	string getNomeP(int IDproduto){//retorna o nome do produto através da ID dele
 		eProduct  *auxiliar;
 		auxiliar = new eProduct;
 		auxiliar = tlista.inicio;
@@ -150,7 +147,7 @@ public:
 		}
 	}
 	
-	int getPrecoP(int IDproduto){
+	int getPrecoP(int IDproduto){//retorna o preço do produtoo ''
 		eProduct  *auxiliar;
 		auxiliar = new eProduct;
 		auxiliar = tlista.inicio;
@@ -162,7 +159,7 @@ public:
 		}		
 	}
 	
-	virtual ~produto(){
+	virtual ~produto(){//destrutor da classe
 		delete tlista.inicio;
 		delete tlista.fim;
 	}
@@ -171,12 +168,12 @@ public:
 
 class cliente: public produto{
 protected:
-	char cpf[12];
-	CONTA *contaCliente;
-	LISTAP *listaDocliente;
-	string nomeC;
+	char cpf[12]; //respectivo cpf
+	CONTA *contaCliente;  //conta do cliente
+	LISTAP *listaDocliente; //lista no nome do cliente
+	string nomeC; //nome do cliente
 public:
-   cliente() {
+   cliente() { //construtor da classe
 			nomeC = "";
 			listaDocliente = NULL;
 			contaCliente = NULL;
@@ -191,65 +188,148 @@ public:
 	    cout << "Informe o CPF:\n" << endl;
 	    getchar();
 		fgets(cpf,12,stdin);
-		cout << "Informe o nome de usu�rio:\n" << endl;
+		cout << "Informe o nome de usuário:\n" << endl;
         cin >> contaCliente->user;
-        cout << "Informe a senha (no m�nimo 6 caracteres):\n" << endl;
+        cout << "Informe a senha (no mínimo 6 caracteres):\n" << endl;
         do{
             cin >> contaCliente->password;
         }while((contaCliente->password.size()) <= 6);
+        changeClientes(0); //soma no total de clientes registrados na empresa
         cout << "Operacao executada com sucesso.\n" << endl;
 	}
 	
 	void carrinhoCliente(){
 		int option;
+		bool flag; //verificador para esvaziar o carrinho
 		LISTAP *carrinho;
-		eProduct *newProd;
+		eProduct *newProd,*aux,*aux2; //adicionar novo produto /auxiliar para percorrer a lista/ auxiliar para esvaziar o carrinho
 		newProd = new eProduct;
+		aux = new eProduct;
 		catalogoProduto();
 		cout << "0)Finalizar compra" << endl;
 		cout << "1)Colocar produtos no carrinho" << endl;
 		cout << "2)Remover produto do carrinho" << endl;
-		cout << "3)Cancelar compra" << endl;
+		cout << "3)Esvaziar carrinho" << endl;
 		cin >> option;
 		while(option != 0){
-			switch(option){
-				case 1:
+			switch(option){	
+				case 1://adiciona um produto no carrinho
 					cout << "Informe a ID do produto: " << endl;
-					cin >> newProd->IDproduto;
-					if(!existe(newProd->IDproduto)){
-						cout << "ID do produto n�o est� presente no cat�logo!" << endl;
-						cout << "Informe novamente!" << endl;
+					cin >> newProd->IDproduto; //lê a ID
+					if(!existe(newProd->IDproduto)){ //verifica se a ID existe no catálogo
+						cout << "ID do produto não está presente no catálogo!" << endl;
 						break;
 					}
 					cout << "Informe a quantidade desse produto: " << endl;					
 					do{
 						cin >> newProd->QTD;
 						if(newProd->QTD <= 0){
-							cout << "Quantidade inv�lida, informe novamente:";
+							cout << "Quantidade inválida, informe novamente:";
 						}
 						cout << "\n";
-					}while(newProd->QTD <= 0);
-					newProd->nomeP = getNomeP(newProd->IDproduto);
-					newProd->preco = getPrecoP(newProd->IDproduto);
+					}while(newProd->QTD <= 0);//verifica se a quantidade é um valor correto
+					newProd->nomeP = getNomeP(newProd->IDproduto); //associa o nome pela ID do produto do catálogo
+					newProd->preco = getPrecoP(newProd->IDproduto);//associa o preço ''
 					newProd->proximo = NULL;
 					newProd->anterior = NULL;
-					if(carrinho->inicio == NULL ){
-						carrinho->inicio = newProd;
-						carrinho->fim = newProd;
-					}else{
+					if(carrinho->inicio == NULL ){ //se não tiver itens no carrinho
+						carrinho->inicio = newProd; //inicio vira o produto
+						carrinho->fim = newProd; // fim vira o produto
+					}else{// se já tiver produtos no carrinho, acrescenta no fim
 						newProd->anterior = carrinho->fim;
 						carrinho->fim->proximo = newProd;
 						carrinho->fim = newProd;
 					}
 					break;
-				case 2:
+				case 2: //Remover produto do carrinho
+					system("cls");
+					int remove;
+					aux = carrinho->inicio;
+					cout << "~Produtos no carrinho~" << endl;
+					while(aux != NULL){ //apressenta os produtos que já estão no carrinho
+						cout << aux->IDproduto << "	|	" << aux->nomeP << "	|	" << aux->QTD << "	|	" <<(aux->preco * aux->QTD);
+						aux = aux->proximo;
+					}
+					cout << "Informe a ID do produto a ser removido: ";
+					cin >> remove; // le a ID do produto
+					aux = carrinho->inicio;
+					while(aux != NULL){
+						if(carrinho->inicio->proximo == NULL && aux->IDproduto == remove){ //se só tiver um elemento no carrinho e a pessoa digitou a ID correta
+							carrinho->inicio = NULL;
+							carrinho->fim = NULL;
+							cout << "Produto removido com sucesso!" << endl;
+							break;
+						}
+						if(aux->IDproduto == remove){// se a ID for correta
+							if(aux == carrinho->inicio){ // se a ID for do primeiro item do carrinho
+								carrinho->inicio = aux->proximo;
+								aux->proximo->anterior = NULL;
+								free(aux);
+								cout << "Produto removido com sucesso!" << endl;
+								break;
+							}else if(aux == carrinho->fim){//se a ID for do ultimo item do carrinho
+								carrinho->fim = aux->anterior;
+								aux->anterior->proximo = NULL;
+								free(aux);
+								cout << "Produto removido com sucesso!" << endl;
+								break;
+							}else{ //se a ID for de um produto no meio do carrinho
+								aux->anterior->proximo = aux->proximo;
+								aux->proximo->anterior = aux->anterior;
+								free(aux);
+								cout << "Produto removido com sucesso!" << endl;
+								break;
+							}
+						}else{
+							aux = aux->proximo;
+						}
+					}
+					break;
+				case 3: //esvazia o carrinho
+					system("cls");
+					cout << "Deseja mesmo cancelar a compra? (1) Sim (0) Não" << endl;
+					cin >> flag;
+					if(flag && carrinho->inicio != NULL){
+						aux = carrinho->inicio;
+						while(aux != NULL){
+							aux2 = aux;
+							aux = aux->proximo;
+							free(aux2);
+						}
+						carrinho->inicio = NULL;
+						carrinho->fim = NULL;
+						cout << "Carrinho esvaziado!" << endl;
+					}
 					break;
 			}
-					
+			catalogoProduto();
+			cout << "0)Finalizar compra" << endl;
+			cout << "1)Colocar produtos no carrinho" << endl;
+			cout << "2)Remover produto do carrinho" << endl;
+			cout << "3)Esvaziar carrinho" << endl;
+			cin >> option;
 		}
+		aux2 = carrinho->inicio;
+		while(aux2 != NULL){
+			for(int cont = 0;cont < aux2->QTD;cont++){
+				changeJogos(1); //reduz a quantidade total de jogos disponiveis na empresa
+			}
+			aux2 = aux2->proximo;
+		}
+		aux2 = carrinho->inicio;
+		if(listaDocliente->inicio == NULL && carrinho->inicio != NULL){//se a lista não tiver produto e o carrinho tiver produtos
+			listaDocliente = carrinho; //a lista recebe os produtos do carrinho
+		}else if(listaDocliente->inicio != NULL && carrinho->inicio != NULL){ // se a lista ja tiver produto
+			while(aux2 != NULL){ // coloca os itens do carrinho no fim da lista de produtos locados pelo cliente
+				listaDocliente->fim->proximo = aux2;
+				aux2->anterior = listaDocliente->fim;
+				listaDocliente->fim = aux2;
+				aux2 = aux2->proximo;
+			}
+		}		
 	}
 	
-	~cliente(){
+	~cliente(){ //destrutor da classe
 	    delete[] contaCliente;
 	    delete[] listaDocliente;
 	}
@@ -257,18 +337,18 @@ public:
 
 class funcionario: public cliente{
 protected:
-    CARGO *IDcargo;
-	int IDfuncionario;
-	CONTA *contaFuncionario;
-	string nomeF;
+    CARGO *IDcargo; //ID do respectivo cargo do funcionario
+	int IDfuncionario; //ID do funcionario
+	CONTA *contaFuncionario;//conta do funcionario
+	string nomeF;//nome do funcionario
 public:
-	funcionario () {
+	funcionario () {//construtor da classe
 	        IDcargo = NULL;
 			IDfuncionario = 0;
 			nomeF = " ";
 			contaFuncionario = NULL;
 	}
-	virtual ~funcionario(){
+	~funcionario(){ //destrutor da classe
 	    delete[] IDcargo;
 	    delete[] contaFuncionario;
 	}
